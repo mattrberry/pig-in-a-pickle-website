0119f9f14696b67818438ef77bc6518cbc6b8d2c
Matthew Berry <me@mattrb.com>
2017-08-08 13:21:09 -0400

    vue cli

---
+++ src/components/Nav.vue
@@ -0,0 +1,21 @@
+<template id="nav">
+  <ol>
+    <li v-for="page in pages">
+      <a v-bind:href="page.url">{{page.name}}</a>
+    </li>
+  </ol>
+</template>
+
+<script>
+export default {
+  name: 'nav',
+  data: function () {
+    return {
+      pages: [
+        { url: '/', name: 'Home' },
+        { url: '/about', name: 'About' }
+      ]
+    }
+  }
+}
+</script>
