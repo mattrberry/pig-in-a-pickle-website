618b3f36d05fb9bb6e6a61a8b9f60cec2ccf74cb
Matthew Berry <me@mattrb.com>
2017-08-20 23:30:37 -0700

    potential mobile solution

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,22 +1,42 @@
 <template>
   <div class="menu">
     <h1><span class="red">All our meats are Hormone & Antibiotic Free, Humanely Raised and Locally Sourced when possible, and are smoked with California White Oak.</span></h1>
+
     <div class="blocks">
-      <div v-for="block in menu_blocks" v-bind:id="block.name">
+      <div v-for="block in menu_blocks" v-bind:id="block.name" class="block tab">
+        <input v-bind:id="classAddTab(block.name)" type="checkbox" name="tabs" class="block-checkbox">
+        <label v-bind:for="classAddTab(block.name)" class="block-name">{{block.name}}</label>
+        <div class="tab-content item" v-for="item in block.items">
+          <p class="item-name">{{item.name}}</p>
+          <p class="item-details">{{item.details}}</p>
+          <p class="item-price">{{item.price}}</p>
+          <br>
+        </div>
+      </div>
+    </div>
+
+    <!--<div class="blocks">
+      <div v-for="block in menu_blocks" v-bind:id="block.name" class="block">
         <h3>{{block.name}}</h3>
         <div class="item" v-for="item in block.items">
           <p class="item-name">{{item.name}}</p>
           <p class="item-details">{{item.details}}</p>
           <p class="item-price">{{item.price}}</p>
+          <br>
         </div>
       </div>
-    </div>
+    </div>-->
   </div>
 </template>
 
 <script>
 export default {
   name: 'menu',
+  methods: {
+    classAddTab: function (nameToAdd) {
+      return 'tab-' + nameToAdd
+    }
+  },
   data: function () {
     return {
       menu_blocks: [
@@ -45,14 +65,31 @@ export default {
             },
             {
               name: 'House Made Hot Link',
-              details: '',
               price: '$6 each (approx 1/3lb)'
             }
           ]
         },
         {
           name: 'SIDES',
-          items: ['potato salad', 'coleslaw', 'beans', 'mac', 'chili', 'greens', 'cornbread', 'rolls', 'chips', 'dill', 'pickle']
+          items: [
+            {
+              name: 'Potato Salad',
+              price: '$3, $6, $9'
+            },
+            {
+              name: 'Coleslaw',
+              price: '$3, $6, $9'
+            },
+            {
+              name: 'Ranch Style Beans',
+              price: '$3, $6, $9'
+            },
+            {
+              name: 'Beef Brisket Chili',
+              details: '~ when available ~',
+              price: '$4, $8, $12'
+            }
+          ]
         },
         {
           name: 'SANDWITCHES',
@@ -68,6 +105,98 @@ export default {
 </script>
 
 <style scoped>
+@media (min-width: 600px) {
+  .block {
+    float: left;
+    padding: 8px;
+  }
+  .block-checkbox {
+    display: none;
+  }
+  .block-name {
+    font-size: 26px;
+    font-weight: bold;
+  }
+}
+
+@media (max-width: 599px) {
+  .block {
+    padding: 0px;
+  }
+  /* Acordeon styles */
+  .tab {
+    //position: relative;
+    margin-bottom: 1px;
+    width: 100%;
+    //color: #fff;
+    //overflow: hidden;
+  }
+  input {
+    position: absolute;
+    opacity: 0;
+    z-index: -1;
+  }
+  label {
+    position: relative;
+    display: block;
+    padding: 0 0 0 1em;
+    //background: #16a085;
+    font-weight: bold;
+    line-height: 3;
+    cursor: pointer;
+
+    text-align: left;
+    font-size: 24px;
+  }
+  .blue label {
+    //background: #2980b9;
+  }
+  .tab-content {
+    max-height: 0;
+    overflow: hidden;
+    //background: #1abc9c;
+    -webkit-transition: max-height .35s;
+    -o-transition: max-height .35s;
+    transition: max-height .35s;
+  }
+  .blue .tab-content {
+    //background: #3498db;
+  }
+  .tab-content p {
+    margin: 1em;
+  }
+  /* :checked */
+  input:checked ~ .tab-content {
+    max-height: 10em;
+  }
+  /* Icon */
+  label::after {
+    position: absolute;
+    right: 0;
+    top: 0;
+    display: block;
+    width: 3em;
+    height: 3em;
+    line-height: 3;
+    text-align: center;
+    -webkit-transition: all .35s;
+    -o-transition: all .35s;
+    transition: all .35s;
+  }
+  input[type=checkbox] + label::after {
+    content: "+";
+  }
+  input[type=radio] + label::after {
+    content: "\25BC";
+  }
+  input[type=checkbox]:checked + label::after {
+    transform: rotate(315deg);
+  }
+  input[type=radio]:checked + label::after {
+    transform: rotateX(180deg);
+  }
+}
+
 .item p {
   margin: 2px;
 }
