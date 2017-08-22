4e59ade7843388fc36d18e1ad14aab9d5ae14c58
Matthew Berry <me@mattrb.com>
2017-08-22 00:37:05 -0700

    updated menu for mobile. still shit on desktop.

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="menu">
     <h3><span class="red">All our meats are Hormone & Antibiotic Free, Humanely Raised and Locally Sourced when possible, and are smoked with California White Oak.</span></h3>
-    <div class="blocks">
+    <!-- <div class="blocks">
       <div v-for="block in menu_blocks" v-bind:id="block.name" class="block tab">
         <input v-bind:id="classAddTab(block.name)" type="checkbox" name="tabs" class="block-checkbox">
         <label v-bind:for="classAddTab(block.name)" class="block-name">{{block.name}}</label>
@@ -10,6 +10,22 @@
           <p class="item-details">{{item.details}}</p>
           <p class="item-price">{{item.price}}</p>
           <br>
+        </div>
+      </div>
+    </div> -->
+    <div class="blocks">
+      <div v-for="block in menu_blocks" v-bind:id="block.name" class="block tab">
+        <input v-bind:id="classAddTab(block.name)" type="checkbox" name="tabs" class="block-checkbox">
+        <label v-bind:for="classAddTab(block.name)" class="block-name">{{block.name}}</label>
+        <div class="tab-content item" v-for="item in block.items">
+          <h4 class="item-name">{{item.name}}<strong class="price" v-if="objSize(item.prices) == 1">{{item.prices[0].value}}</strong></h4>
+          <p class="item-details">{{item.details}}</p>
+          <!-- <div v-for="price in item.prices">
+            <p class="item-prices">{{price.label}}<strong class="price">{{price.value}}</strong></p>
+            <hr/>
+          </div> -->
+          <p class="item-prices" v-if="objSize(item.prices) > 1" v-for="price in item.prices">{{price.label}}<strong class="price">{{price.value}}</strong></p>
+          <!-- <br> -->
         </div>
       </div>
     </div>
@@ -22,188 +38,457 @@ export default {
   methods: {
     classAddTab: function (nameToAdd) {
       return 'tab-' + nameToAdd
+    },
+    objSize: function (obj) {
+      try {
+        return Object.keys(obj).length
+      } catch (e) {
+        return 0
+      }
     }
   },
   data: function () {
     return {
       menu_blocks: [
         {
-          name: 'MEATS',
+          name: 'Meats',
           items: [
             {
               name: 'Baby Back Ribs',
               details: 'Dry Rubbed and Lightly Smoked',
-              price: '$9 3 ribs, $18 6 ribs, $30 12 ribs'
+              prices: [
+                {
+                  label: '3 ribs',
+                  value: '9'
+                },
+                {
+                  label: '6 ribs',
+                  value: '18'
+                },
+                {
+                  label: '12 ribs',
+                  value: '30'
+                }
+              ]
             },
             {
               name: 'Smoked Chicken',
               details: 'Tossed in Alabama White Sauce',
-              price: '$8 1/4 chicken, $12 1/2 chicken, $24 whole chicken'
+              prices: [
+                {
+                  label: '1/4 chicken',
+                  value: '8'
+                },
+                {
+                  label: '1/2 chicken',
+                  value: '12'
+                },
+                {
+                  label: '1 chicken',
+                  value: '24'
+                }
+              ]
             },
             {
               name: 'Beef Brisket',
               details: 'Wet Rubbed & Smoked for 18 hours',
-              price: '$8 1/4lb, $15 1/2lb, $28 1lb'
+              prices: [
+                {
+                  label: '1/4 pound',
+                  value: '8'
+                },
+                {
+                  label: '1/2 pound',
+                  value: '15'
+                },
+                {
+                  label: '1 pound',
+                  value: '28'
+                }
+              ]
             },
             {
               name: 'Pulled Pork',
               details: 'Dry Rubbed Smoked Pork Shoulder',
-              price: '$6 1/4lb, $10 1/2lb, $18 1lb'
+              prices: [
+                {
+                  label: '1/4 pound',
+                  value: '6'
+                },
+                {
+                  label: '1/2 pound',
+                  value: '10'
+                },
+                {
+                  label: '1 pound',
+                  value: '18'
+                }
+              ]
             },
             {
               name: 'House Made Hot Link',
-              price: '$7 each (approx 1/3lb)'
+              prices: [
+                {
+                  label: 'Per Link',
+                  value: '7'
+                }
+              ]
             }
           ]
         },
         {
-          name: 'SIDES',
+          name: 'Sides',
           items: [
             {
               name: 'Potato Salad',
-              price: '$3.50 8oz, $7 16oz, $11 32oz'
+              prices: [
+                {
+                  label: '8oz',
+                  value: '3.50'
+                },
+                {
+                  label: '16oz',
+                  value: '7'
+                },
+                {
+                  label: '32oz',
+                  value: '11'
+                }
+              ]
             },
             {
               name: 'Coleslaw',
-              price: '$3 8oz, $6 16oz, $9 32oz'
+              prices: [
+                {
+                  label: '8oz',
+                  value: '3'
+                },
+                {
+                  label: '16oz',
+                  value: '6'
+                },
+                {
+                  label: '32oz',
+                  value: '9'
+                }
+              ]
             },
             {
               name: 'Ranch Style Beans',
-              price: '$3.50 8oz, $7 16oz, $11 32oz'
+              prices: [
+                {
+                  label: '8oz',
+                  value: '3.50'
+                },
+                {
+                  label: '16oz',
+                  value: '7'
+                },
+                {
+                  label: '32oz',
+                  value: '11'
+                }
+              ]
             },
             {
               name: 'Beef Brisket Chili',
-              details: '~ when available ~',
-              price: '$5 8oz, $9 16oz, $16 32oz'
+              details: '~ when available',
+              prices: [
+                {
+                  label: '8oz',
+                  value: '5'
+                },
+                {
+                  label: '16oz',
+                  value: '9'
+                },
+                {
+                  label: '32oz',
+                  value: '16'
+                }
+              ]
             },
             {
               name: 'Collard Greens w/ Ham Hock',
-              price: '$4 8oz, $8 16oz, $14 32oz'
+              prices: [
+                {
+                  label: '8oz',
+                  value: '4'
+                },
+                {
+                  label: '16oz',
+                  value: '8'
+                },
+                {
+                  label: '32oz',
+                  value: '14'
+                }
+              ]
             },
             {
               name: 'Vegetarian Collard Greens',
-              price: '$4 8oz, $8 16oz, $14 32oz'
+              prices: [
+                {
+                  label: '8oz',
+                  value: '4'
+                },
+                {
+                  label: '16oz',
+                  value: '8'
+                },
+                {
+                  label: '32oz',
+                  value: '14'
+                }
+              ]
             },
             {
               name: 'Cornbread',
-              price: '$3'
+              prices: [
+                {
+                  label: 'Per Piece',
+                  value: '3'
+                }
+              ]
             },
             {
               name: 'Parker House Rolls',
-              price: '$4 for 4 small rolls'
+              prices: [
+                {
+                  label: '4 small rolls',
+                  value: '4'
+                }
+              ]
             },
             {
               name: 'Kettle Tallow Chips',
-              price: '$2 side, $4 bag'
+              prices: [
+                {
+                  label: 'Side',
+                  value: '2'
+                },
+                {
+                  label: 'Bag',
+                  value: '4'
+                }
+              ]
             },
             {
               name: 'Dill Pickle',
-              price: '$2'
+              prices: [
+                {
+                  label: 'Per Pickle',
+                  value: '2'
+                }
+              ]
             },
             {
               name: 'B & B Pickles',
-              price: '$3 side, $7 jar'
+              prices: [
+                {
+                  label: 'Side',
+                  value: '3'
+                },
+                {
+                  label: 'Jar',
+                  value: '7'
+                }
+              ]
             },
             {
               name: 'To Go Sauces',
               details: '2oz per 1/4lb of Meat is Included',
-              price: '$2 4oz, $7 jar'
+              prices: [
+                {
+                  label: '4oz',
+                  value: '2'
+                },
+                {
+                  label: 'Jar',
+                  value: '7'
+                }
+              ]
             }
           ]
         },
         {
-          name: 'SANDWITCHES',
+          name: 'Sandwitches',
           items: [
             {
               name: 'Brisket Sandwitch',
-              price: '$14'
+              prices: [
+                {
+                  label: 'Per Sandwitch',
+                  value: '14'
+                }
+              ]
             },
             {
               name: 'Pulled Pork Sandwitch',
-              price: '$12'
+              prices: [
+                {
+                  label: 'Per Sandwitch',
+                  value: '12'
+                }
+              ]
             },
             {
               name: 'Chicken Salad',
               details: 'Pulled Chicken, Parsley & Celery Tossed in Alabama White Sauce With Caramelized Onions & Romaine Hearts',
-              price: '$12'
+              prices: [
+                {
+                  label: 'Per Sandwitch',
+                  value: '12'
+                }
+              ]
             },
             {
               name: 'Hot Link Sandwitch',
               details: 'With Roasted Sweet Peppers & Caramelized Onions',
-              price: '$11'
+              prices: [
+                {
+                  label: 'Per Sandwitch',
+                  value: '11'
+                }
+              ]
             },
             {
               name: 'KIDS Beef Hot Dog',
-              price: '$6'
+              prices: [
+                {
+                  label: 'Per Sandwitch',
+                  value: '6'
+                }
+              ]
             },
             {
               name: 'KIDS Brisket Sandwitch',
-              price: '$7'
+              prices: [
+                {
+                  label: 'Per Sandwitch',
+                  value: '7'
+                }
+              ]
             },
             {
               name: 'KIDS Pork Sandwitch',
-              price: '$6'
+              prices: [
+                {
+                  label: 'Per Sandwitch',
+                  value: '6'
+                }
+              ]
             },
             {
               name: 'KIDS Pulled Chicken Sandwitch',
               details: '(cold)',
-              price: '$6'
+              prices: [
+                {
+                  label: 'Per Sandwitch',
+                  value: '6'
+                }
+              ]
             }
           ]
         },
         {
-          name: 'SALADS',
+          name: 'Salads',
           items: [
             {
               name: 'Seasonal Mixed Greens',
-              price: '$9'
+              prices: [
+                {
+                  label: 'Per Salad',
+                  value: '9'
+                }
+              ]
             },
             {
-              name: 'with Pulled Chicken',
-              price: '$12'
+              name: '-> with Pulled Chicken',
+              prices: [
+                {
+                  label: 'Per Salad',
+                  value: '12'
+                }
+              ]
             },
             {
-              name: 'with Pulled Pork',
-              price: '$14'
+              name: '-> with Pulled Pork',
+              prices: [
+                {
+                  label: 'Per Salad',
+                  value: '14'
+                }
+              ]
             },
             {
-              name: 'with Beef Brisket',
-              price: '$16'
+              name: '-> with Beef Brisket',
+              prices: [
+                {
+                  label: 'Per Salad',
+                  value: '16'
+                }
+              ]
             }
           ]
         },
         {
-          name: 'PLATES & PLATTERS',
+          name: 'Plates & Platters',
           items: [
             {
               name: '2 Meats & 3 Sides',
               details: '1/2lb of Meat',
-              price: '$19'
+              prices: [
+                {
+                  label: 'Per Plate',
+                  value: '19'
+                }
+              ]
             },
             {
               name: '3 Meats & 3 Sides',
               details: '3/4lb of Meat',
-              price: '$25'
+              prices: [
+                {
+                  label: 'Per Plate',
+                  value: '25'
+                }
+              ]
             },
             {
               name: '3 Meats & 4 Sides',
               details: '1.5lb of Meat',
-              price: '$65'
+              prices: [
+                {
+                  label: 'Per Platter',
+                  value: '65'
+                }
+              ]
             }
           ]
         },
         {
-          name: 'DESSERTS',
+          name: 'Desserts',
           items: [
             {
               name: 'Scoop of Three Twins Vanilla',
               details: 'with House Made Caramel Sauce',
-              price: '$3'
+              prices: [
+                {
+                  label: 'Per Scoop',
+                  value: '3'
+                }
+              ]
             },
             {
               name: 'Seasonal Selections',
               details: 'add $1 for Three Twins Vanilla Ice Cream on Top',
-              price: '$6'
+              prices: [
+                {
+                  label: 'Per Dessert',
+                  value: '6'
+                }
+              ]
             }
           ]
         },
@@ -213,32 +498,62 @@ export default {
             {
               name: 'Cola / Diet Cola / Lemon-Lime / Ginger Ale / Ice Tea / Sweet Tea / Arnold Palmer',
               details: 'Free Refills',
-              price: '$3'
+              prices: [
+                {
+                  label: 'Per Drink',
+                  value: '3'
+                }
+              ]
             },
             {
               name: 'Coffee / Decaf',
               details: '$1 Refills',
-              price: '$3'
+              prices: [
+                {
+                  label: 'Per Coffee',
+                  value: '3'
+                }
+              ]
             },
             {
               name: 'Lemonade',
               details: '$1 Refills',
-              price: '$3'
+              prices: [
+                {
+                  label: 'Per Lemonade',
+                  value: '3'
+                }
+              ]
             },
             {
               name: 'Strawberry Lemonade',
               details: '$1 Refills',
-              price: '$3.50'
+              prices: [
+                {
+                  label: 'Per Lemonade',
+                  value: '3.50'
+                }
+              ]
             },
             {
               name: 'Sprechers Root Beer',
               details: 'Bottle',
-              price: '$4'
+              prices: [
+                {
+                  label: 'Per Bottle',
+                  value: '4'
+                }
+              ]
             },
             {
               name: 'Marin Kombucha',
               details: 'Bottle',
-              price: '$5'
+              prices: [
+                {
+                  label: 'Per Bottle',
+                  value: '5'
+                }
+              ]
             }
           ]
         }
@@ -273,8 +588,6 @@ export default {
     //position: relative;
     margin-bottom: 1px;
     width: 100%;
-    //color: #fff;
-    //overflow: hidden;
   }
   input {
     position: absolute;
@@ -285,7 +598,6 @@ export default {
     position: relative;
     display: block;
     padding: 0 0 0 1em;
-    //background: #16a085;
     font-weight: bold;
     line-height: 3;
     cursor: pointer;
@@ -293,22 +605,12 @@ export default {
     text-align: left;
     font-size: 24px;
   }
-  .blue label {
-    //background: #2980b9;
-  }
   .tab-content {
     max-height: 0;
     overflow: hidden;
-    //background: #1abc9c;
     -webkit-transition: max-height .35s;
     -o-transition: max-height .35s;
     transition: max-height .35s;
-  }
-  .blue .tab-content {
-    //background: #3498db;
-  }
-  .tab-content p {
-    margin: 1em;
   }
   /* :checked */
   input:checked ~ .tab-content {
@@ -342,19 +644,46 @@ export default {
   }
 }
 
-.item p {
-  margin: 2px;
+.block-name {
+  text-transform: uppercase;
 }
 
 .item-name {
+  text-align: left;
+  margin: 2px;
   font-size: 26px;
 }
 
-.item-details {
-  font-size: 20px;
+.item {
+  padding: 0 0 0 24px;
 }
 
-.item-price {
-  font-size: 24px;
+.item-details {
+  text-align: left;
+  font-size: 20px;
+  font-style: italic;
+  margin: -5px 2px 2px 2px;
 }
+
+.item-prices {
+  text-align: left;
+  font-size: 20px;
+  margin: 2px;
+}
+
+.price:before {
+  content: '$';
+}
+
+.price {
+  float: right;
+  padding: 0 24px 0 0;
+  font-size: 18px;
+}
+
+/*hr {
+  height: 0px;
+  position: relative;
+  margin: -3px 0 0 0;
+}*/
 </style>
