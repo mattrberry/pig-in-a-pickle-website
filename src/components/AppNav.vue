594e85747f69281278f413878b6397764d999530
Matthew Berry <me@mattrb.com>
2017-08-24 00:53:59 -0700

    animated dropdown tray for mobile

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -1,7 +1,13 @@
 <template id="nav">
   <div id="nav">
     <router-link id="nav-home" to="/">Pig in a Pickle</router-link>
-    <p v-for="page in pages"><router-link v-bind:to="page.url">{{page.name}}</router-link></p>
+    <label for="menu-toggle"></label>
+    <input type="checkbox" id="menu-toggle" />
+    <div id="menu-items">
+      <p class="menu-item" onclick="document.getElementById('menu-toggle').checked = false;" v-for="page in pages">
+        <router-link v-bind:to="page.url">{{page.name}}</router-link>
+      </p>
+    </div>
   </div>
 </template>
 
@@ -21,32 +27,66 @@ export default {
 </script>
 
 <style scoped>
+@media (min-width: 801px) {
+  #menu-items {
+    display: inline-block;
+  }
+  #menu-toggle, label {
+    display: none;
+  }
+  .menu-item {
+    display: inline-block;
+  }
+}
+@media (max-width: 800px) {
+  #menu-toggle {
+    clip: rect(0, 0, 0, 0);
+  }
+  label[for="menu-toggle"]:before, #menu-toggle {
+    content: "\2630";
+    width: 50px;
+    height: 50px;
+    right: 0;
+    line-height: 50px;
+    position: fixed;
+    margin: 0;
+    padding: 0;
+  }
+  #menu-items {
+    margin-top: -100vh;
+    transition: all .4s ease-in;
+  }
+  #menu-toggle:checked + #menu-items {
+    margin-top: 0;
+    transition: all .4s ease-out;
+  }
+  .menu-item {
+    display: block;
+  }
+}
+
 #nav {
   background: #964F4C;
-  //overflow: hidden;
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
-  height: 50px;
   text-align: center;
-
-  font-size: 24px;
-
+  min-height: 50px;
   z-index: 1;
+  font-size: 24px;
 }
-#nav p, #nav-home {
-  //float: left;
+.menu-item, #nav-home {
   margin: 0;
   padding: 2px 4px 2px 4px;
   text-decoration: none;
   color: #FFF;
 
   vertical-align: middle;
-  display: inline-block;
 }
 #nav-home {
   font-size: 36px;
+  display: inline-block;
 }
 #nav a {
   text-decoration: none;
