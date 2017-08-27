7a0a9cddc6b8f832562dafb805eeb7930513f388
Matthew Berry <me@mattrb.com>
2017-08-27 00:28:15 -0700

    three new pages -> ALL ARE STILL WORKS IN PROGRESS <-

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,9 @@ import Router from 'vue-router'
 import Home from '@/components/Home'
 import About from '@/components/About'
 import Menu from '@/components/Menu'
+import Catering from '@/components/Catering'
+import Delivery from '@/components/Delivery'
+import FindUs from '@/components/FindUs'
 
 Vue.use(Router)
 
@@ -23,6 +26,21 @@ export default new Router({
       path: '/menu',
       name: 'Menu',
       component: Menu
+    },
+    {
+      path: '/catering',
+      name: 'Catering',
+      component: Catering
+    },
+    {
+      path: '/delivery',
+      name: 'Delivery',
+      component: Delivery
+    },
+    {
+      path: '/findus',
+      name: 'Find Us',
+      component: FindUs
     }
   ],
   scrollBehavior (to, from, savedPosition) {
