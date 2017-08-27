40725f5bffb1f6738ebf80617611a2f68ba7c47c
Matthew Berry <me@mattrb.com>
2017-08-27 00:27:47 -0700

    beginnings of a find us page

---
+++ src/components/FindUs.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="findus">
+    <p class="first-line">We are looking forward to your visit with us. And if you want to just stop in and say hi, we’d like that too!</p>
+<div class="address">
+    <h3 class="center">Pig in a Pickle</h3>
+    <p>341 Corte Madera Town Center</p>
+    <p>Corte Madera, CA 94925</p>
+  </div>
+    <iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyB3W6i-NUhGAz8aE1h420mk2pbdOFT94Hw&q=Pig+in+a+Pickle+BBQ,Corte+Madera+CA" allowfullscreen></iframe>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'findus'
+}
+</script>
+
+<style scoped>
+.address {
+  margin-bottom: 10px;
+}
+.address p, .address h3 {
+  margin: 2px;
+  padding: 0;
+  line-height: 24px;
+}
+iframe {
+  width: 90%;
+  height: 400px;
+  border: 2px solid #FFF;
+}
+</style>
