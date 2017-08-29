16ac7811d0c561908758ce251357478dd22f7780
Matthew Berry <me@mattrb.com>
2017-08-29 16:46:54 -0700

    formatting

--- src/components/FindUs.vue
+++ src/components/FindUs.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="findus">
     <p class="first-line">We are looking forward to your visit with us. And if you want to just stop in and say hi, we’d like that too!</p>
-<div class="address">
-    <h3 class="center">Pig in a Pickle</h3>
-    <p>341 Corte Madera Town Center</p>
-    <p>Corte Madera, CA 94925</p>
-  </div>
+    <div class="address">
+      <h3 class="center">Pig in a Pickle</h3>
+      <p>341 Corte Madera Town Center</p>
+      <p>Corte Madera, CA 94925</p>
+    </div>
     <iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyD8Pqp0WEP8v-jNQuNto6TydWc8WyVlwfQ&q=Pig+in+a+Pickle+BBQ,Corte+Madera+CA" allowfullscreen></iframe>
-</div>
+  </div>
 </template>
 
 <script>
