8518cf4b94d005dce1135eb626372e882bca8224
Matthew Berry <me@mattrb.com>
2017-09-16 11:36:54 -0400

    images now fade transition

--- src/components/Catering.vue
+++ src/components/Catering.vue
@@ -3,9 +3,12 @@
   <h3 class="first-line"><span class="red center">We can customize any event to your specific needs, so let’s start with the basics...</span></h3>
   <h3><span class="center">Catering office hours Monday- Friday 9am to 4pm</span></h3>
   <h3><span class="center">Reach us at <a href="tel:4158918124"><span class="red">415-891-8124</span></a> or <a href="mailto:piapevents@gmail.com"><span class="red">piapevents@gmail.com</span></a></span></h3>
-  <div id="slides">
-    <img class="slide" v-for="n in 9" :src="imgSrc(n)">
-  </div>
+  <!-- <div class="container"> -->
+    <div id="slides">
+    <img class="slide" id="first" :src="imgSrc(0)">
+      <img v-for="(_, n) in 10" class="slide" :src="imgSrc(n)">
+    </div>
+  <!-- </div> -->
 </div>
 </template>
 
@@ -15,6 +18,10 @@ export default {
   methods: {
     imgSrc: function (num) {
       return '/static/catering/c' + num + '.jpg'
+    },
+    oneHuh: function (num) {
+      // return num.toString()
+      return num === 0 ? 'first' : ''
     },
     carousel: function () {
       try {
@@ -43,22 +50,92 @@ export default {
     }
   },
   mounted: function () {
-    this.carousel()
+    // this.carousel()
   }
 }
 </script>
 
 <style scoped>
-.slide {
+/*.container {
+  overflow: hidden;
+  height: 500px;
+  width: 100%;
+}*/
+img {
+  /*height: 100%;*/
+}
+#first {
+  width: 100%;
+}
+.slide:not(#first) {
   width: 100%;
   top: 0;
+  left: 0;
+  position: absolute;
+  opacity: 0;
+
+  animation: fade-in 40s linear infinite;
 }
+/* 1280x854 */
 #slides {
   border: 2px #FFF solid;
   width: 80%;
   margin: 0 auto;
   margin-top: 10px;
   margin-bottom: 10px;
+  position: relative;
+
+  /*height: 100%;
+  width: 12800px;*/
+}
+
+#slides > .slide:nth-child(2) {
+  animation-delay: 0s;
+}
+#slides > .slide:nth-child(3) {
+  animation-delay: 4s;
+}
+#slides > .slide:nth-child(4) {
+  animation-delay: 8s;
+}
+#slides > .slide:nth-child(5) {
+  animation-delay: 12s;
+}
+#slides > .slide:nth-child(6) {
+  animation-delay: 16s;
+}
+#slides > .slide:nth-child(7) {
+  animation-delay: 20s;
+}
+#slides > .slide:nth-child(8) {
+  animation-delay: 24s;
+}
+#slides > .slide:nth-child(9) {
+  animation-delay: 28s;
+}
+#slides > .slide:nth-child(10) {
+  animation-delay: 32s;
+}
+#slides > .slide:nth-child(11) {
+  animation-delay: 36s;
+}
+
+@keyframes "fade-in" {
+  0% {
+    opacity: 0;
+  }
+  5% {
+    opacity: 1;
+  }
+  10% {
+    opacity: 1;
+  }
+  15% {
+    opacity: 1;
+  }
+  16% {
+    opacity: 0;
+  }
 }
 
 h3 {
