659ed0c72df28b4b8194e96e9b94a7ddc72b97e2
Matthew Berry <me@mattrb.com>
2017-09-16 19:18:14 -0400

    email and delivery links updated

--- src/components/Delivery.vue
+++ src/components/Delivery.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="delivery">
     <h3 class="first-line"><span class="red center">It’s not just for pizza or Chinese food anymore!</span></h3>
-    <p>We are now teaming up with Dine-In Marin because they deliver ALL DAY, EVERYDAY!  You can have your Pig in a Pickle BBQ delivered anytime we are open by ordering online at <a href="https://www.dineinmarin.com/order/restaurant/pig-in-a-pickle-menu/83"><span class="red">DineinMarin.com</span></a></p>
-    <p>But Wait…. what about my FREE <strong><i>Office Delivery</i></strong>?  Yes, with 24 hour notice we will still deliver lunch to your office Monday through Friday for orders  over $150. Email your order to <a href="mailto:piapevents@gmail.com"><span class="red">piapevents@gmail.com</span></a></p>
+    <p>We are now teaming up with Dine-In Marin because they deliver ALL DAY, EVERYDAY!  You can have your Pig in a Pickle BBQ delivered anytime we are open by ordering online at <a href="https://www.dineinmarin.com/order/restaurant/pig-in-a-pickle-menu/83"><span class="red">DineInMarin.com</span></a></p>
+    <p>But Wait…. what about my FREE <strong><i>Office Delivery</i></strong>?  Yes, with 24 hour notice we will still deliver lunch to your office Monday through Friday for orders  over $150. Email your order to <a href="mailto:catering@piginapickle.com"><span class="red">catering@piginapickle.com</span></a></p>
     <p>For 20 people or more, check out our <strong><i>Catering Department 415-891-8124</i></strong> for details on delivery and more.</p>
     <img src="/static/delivery/all-that.png">
   </div>
