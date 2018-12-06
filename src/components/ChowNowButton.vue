d056c570a981325b6a3aa4b26968364120d09f48
Matthew Berry <me@mattrb.com>
2018-12-05 22:30:51 -0500

    update to fix button placement

--- src/components/ChowNowButton.vue
+++ src/components/ChowNowButton.vue
@@ -1,18 +1,10 @@
 <template>
-  <div id="chownow-button">
-    <!-- <label for="chownow-toggle" v-on:click="toggleChowNow()" id="chownow-toggle-label"></label> -->
-    <!-- <input type="checkbox" id="chownow-toggle" /> -->
-    <a class="chownow-order-online" href="https://ordering.chownow.com/order/9478/locations" target="_blank">Order Now!</a>
-    <!-- <p class="chownow-order-online">Order Now!</p> -->
-  </div>
+  <a class="chownow-order-online" href="https://ordering.chownow.com/order/9478/locations" target="_blank">Order Now!</a>
 </template>
 
 <script>
-import { ChowNowToggle } from './mixins/ChowNowToggle.js'
-
 export default {
   name: 'chownowbutton',
-  mixins: [ChowNowToggle],
   mounted: function () {
     let chownowScript = document.createElement('script')
     chownowScript.setAttribute('src', 'https://cf.chownowcdn.com/latest/static/integrations/ordering-modal.min.js')
@@ -23,16 +15,10 @@ export default {
 </script>
 
 <style scoped>
-#chownow-toggle {
-  clip: rect(0, 0, 0, 0);
-  display: none;
-}
-label[for="chownow-toggle"]:before, .chownow-order-online {
-  content: "Order Now!";
+.chownow-order-online {
   padding: 6px 18px;
   border: 2px solid #FFFFFF;
   border-radius: 4px;
-  margin: 0 auto;
   font-size: 24px;
   text-decoration: none;
   color: #7D3A38;
@@ -42,7 +28,7 @@ label[for="chownow-toggle"]:before, .chownow-order-online {
 
   font-size: 30px;
 }
-label[for="chownow-toggle"]:hover:before, .chownow-order-online {
+.chownow-order-online:hover {
   color: #FFFFFF;
   background-color: #7D3A38;
 }
